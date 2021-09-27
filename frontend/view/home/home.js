e6de35e6af5e09fa414e07cc80fc0c03e97f265b
nathbrl <70270233+nathbrl@users.noreply.github.com>
2021-09-27 11:27:35 +0200

    1er besoin du projet
    Afficher les derniers articles publiés par le site avec le nom, la date de publication, le contenu (image+texte)
    test pour 1 article

--- frontend/view/home/home.js
+++ frontend/view/home/home.js
@@ -2,6 +2,36 @@
  * Gère l'affichage et les interactions de la page d'accueil
  */
 
-fetch("http://localhost:4000/api/article/")
-    .then(res => res.json())
-    .then(data => console.log(data))
+function getArticles () {
+    fetch("http://localhost:4000/api/article/")
+        .then((res) => res.json())
+        .then(article => (article.forEach(element => {
+                console.log(element);
+                createCard(article);
+            })));
+        
+}
+getArticles();
+
+function createCard(article) {
+    let div = document.createElement('div');
+    let title = document.createElement('h1');
+    let p = document.createElement('p');
+    let img = document.createElement('img');
+    let span = document.createElement('span');
+    let id = document.createElement('id');
+    id.innerHTML = article[0].id;
+    title.innerHTML = article[0].title;
+    p.innerHTML = article[0].content;
+    span.innerHTML = article[0].publicationDate;
+    img.src = article[0].image;
+    console.log(div);
+    let container = document.getElementsByClassName('container');
+    document.body.appendChild(div);
+    div.appendChild(id);
+    div.appendChild(title);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(span);
+}
+createCard();
